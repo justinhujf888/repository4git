12c380aeec127fb00124bbb5ff2e093fa7fb7bc3
Justin <justinhujf@gmail.com>
2025-07-19 01:35:07 +0800

    update

--- webpro/edu/src/api/config.js
+++ webpro/edu/src/api/config.js
@@ -3,12 +3,12 @@ export const Config = {
     vueRouterMode: 'history',
 
     appId: 'wxab8fc59cb9d4f7cb',
-    appName: 'cls',
+    appName: '慧学宝盒',
     appVersion: '0.2',
 
     //接口地址 http://localhost:8080/shop
     // apiBaseURL: "http://192.168.124.24:8090/light",
-    apiBaseURL: import.meta.env.MODE === "production" ?	"" : "http://localhost:8090/light",
+    apiBaseURL: import.meta.env.MODE === "production" ?	"" : "http://localhost:8091/cpt",
 
     desKey: 'vikehoo_public_key_%&%^*&^*',
     txMapKey: 'OWYBZ-QUQ6F-MU6J4-JYBLN-LL7E7-CNBE6'
