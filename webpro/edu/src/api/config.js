c1c002ca3fb05e2ffe984399aa150af9954240e2
Justin <justinhujf@gmail.com>
2025-07-22 02:33:25 +0800

    update

--- webpro/edu/src/api/config.js
+++ webpro/edu/src/api/config.js
@@ -8,7 +8,7 @@ export const Config = {
 
     //接口地址 http://localhost:8080/shop
     // apiBaseURL: "http://192.168.124.24:8090/light",
-    apiBaseURL: import.meta.env.MODE === "production" ?	"" : "http://localhost:8091/cpt",
+    apiBaseURL: import.meta.env.MODE === "production" ?	"" : "http://localhost:8080/edu",
 
     desKey: 'vikehoo_public_key_%&%^*&^*',
     txMapKey: 'OWYBZ-QUQ6F-MU6J4-JYBLN-LL7E7-CNBE6'
