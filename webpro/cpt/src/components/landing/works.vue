315b929387fee35c5d13153ed80453bd4cc99c94
justin <justinhujf@gmail.com>
2025-11-05 15:29:59 +0800

    update

--- webpro/cpt/src/components/landing/works.vue
+++ webpro/cpt/src/components/landing/works.vue
@@ -4,9 +4,12 @@
             <div class="_text-surface-900 _dark:text-surface-0 font-normal mb-2 text-4xl mix-blend-difference text-white">获奖作品</div>
         </div>
         <div class="mt-20 w-full">
-            <div class="box w-full h-60">
-                <div class="imgList flex">
-                    <img v-for="img of imgList" :src="img" class="w-48 flex-1"/>
+            <div class="box w-full" style="height: 45rem;">
+                <div class="imgList left-0">
+                    <div class="col">
+                        <div class="row h-80 w-auto"><img v-for="img of imgList[0]" :src="img" class="flex-1 h-full object-cover object-center"/></div>
+                        <div class="row h-80 w-auto"><img v-for="img of imgList[1]" :src="img" class="flex-1 h-full object-cover object-center"/></div>
+                    </div>
                 </div>
             </div>
 <!--            <DomeGallery-->
@@ -34,16 +37,17 @@ import oss from '@/api/oss';
 // import Masonry from '@/bit-blocks/Components/Masonry/Masonry.vue';
 // import DomeGallery from "@/bit-blocks/Components/DomeGallery/DomeGallery.vue";
 
-const imgList = ref(null);
+const imgList = ref([[],[]]);
 (async ()=>{
     let siteDatas = await useGlobal.siteDatas();
-    imgList.value = [];
     lodash.forEach(siteDatas.siteInfo.siteOrgHumanList,(v)=>{
-        imgList.value.push(oss.buildImgPath(v.headImgUrl));
+        imgList.value[0].push(oss.buildImgPath(v.headImgUrl));
     });
     lodash.forEach(siteDatas.siteInfo.siteJudgeList,(v)=>{
-        imgList.value.push(oss.buildImgPath(v.headImgUrl));
+        imgList.value[1].push(oss.buildImgPath(v.headImgUrl));
     });
+    imgList.value[0] = lodash.concat(imgList.value[0],imgList.value[1]);
+    imgList.value[1] = lodash.concat(imgList.value[1],imgList.value[0]);
 })();
 </script>
 
@@ -56,7 +60,6 @@ const imgList = ref(null);
     .imgList {
         animation:rolling 30s linear infinite;
         position: absolute;
-        display: flex;
     }
 }
 
